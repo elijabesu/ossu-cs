53329fdef23c038485ae0722ac4e9d0ab076f5a8
saurich <ellie@saurich.com>
2020-09-02 18:02:29 +0200

    Moved to playground repo

--- other/enumerate.py
+++
@@ -1,4 +0,0 @@
-days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-
-for i, d in enumerate(days):
-    print("Index", i, "corresponds to", d + ".")
--- other/global.py
+++
@@ -1,9 +0,0 @@
-a = 1
-b = 2
-
-def func():
-    global b # we are calling for the global variable and changing that one
-    b += a
-
-func()
-print(b) # prints 3
